6f6c4a569ef20043e61ae91dbed958696b1c128d
Hudson9700 <42658031+Hudson9700@users.noreply.github.com>
2018-10-30 18:15:53 -0400

    a

--- action.js
+++ action.js
@@ -1,15 +1,29 @@
-function checkEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-
-}
-
-function checkAge() {
-    var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
-    
-    if() {
-       
+document.getElementById("name").onKeypress = function () {
+    var name = document.getElementById("name");
+    var regExp = new RegExp("<");
+    if (regExp.test(document.getElementById("name").value)) {
+        name.style.backgroundColor = "red";
     } else {
-       
+        name.style.backgroundColor = "";
     }
 }
+
+document.getElementById("email").onclick = function () {
+    var regExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');;
+    if(regExp.test(document.getElementById("email").value)){
+        return(true);
+    } else{
+        alert("You have entered a invalid meail");
+        return (false)
+    }
+}
+
+document.getElementById('age').onkeyup = function () {
+    var regExp = new regExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+    if (regExp.test(document.getElementById('age').value)) {
+        name.style.backgroundColor = "red";
+    } else {
+        name.style.backgroundColor = "";
+    }
+}
+
